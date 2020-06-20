33cc2cd39445206d26853b5f1ff5ae62f0f56b22
dootrex <avigill@hotmail.ca>
2020-06-19 19:03:07 -0700

    Allow proxy request

--- server/services/passport.js
+++ server/services/passport.js
@@ -14,27 +14,29 @@ passport.deserializeUser((id, done) => {
     done(null, user);
   });
 });
-passport.use(new FacebookStrategy({
-  clientID: keys.facebookAppID,
-  clientSecret: keys.facebookSecret,
-  callbackURL: "http://localhost:3000/auth/facebook/callback"
-},
-function(accessToken, refreshToken, profile, done) {
-  User.findOne({ facebookId: profile.id }).then((existingUser) => {
-    if (existingUser) {
-      //there is a user with this id
-      done(null, existingUser);
-    } else {
-      //lets make a new user
-      new User({ facebookId: profile.id })
-        .save()
-        .then((user) => done(null, user)); //the user in this instance is the freshest
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookAppID,
+      clientSecret: keys.facebookSecret,
+      callbackURL: "/auth/facebook/callback",
+      proxy: true
+    },
+    function (accessToken, refreshToken, profile, done) {
+      User.findOne({ facebookId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //there is a user with this id
+          done(null, existingUser);
+        } else {
+          //lets make a new user
+          new User({ facebookId: profile.id })
+            .save()
+            .then((user) => done(null, user)); //the user in this instance is the freshest
+        }
+      });
     }
-  });
-
-  
-}
-));
+  )
+);
 
 passport.use(
   new GoogleStrategy(
@@ -42,6 +44,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true,
     },
     (accessToken, refreshToken, profile, done) => {
       //this is creating a model instance and then saving it to DB
